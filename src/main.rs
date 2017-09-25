a3474348e3bf85bf6e8466bda441ca4ff24eb291
David Herman <david.herman@gmail.com>
2017-09-25 14:37:51 -0700

    basic command-line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+extern crate clap;
+
+use clap::{Arg, App, SubCommand};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("Nemo")
+        .version("1.0")
+        .about("The Node toolchain manager")
+
+        // nemo install [version]
+        .subcommand(SubCommand::with_name("install")
+            .about("install the toolchain to the local machine")
+            .arg(Arg::with_name("version")
+                .help("Node.js version specifier")
+                .required(false)))
+
+        // nemo use [version]
+        .subcommand(SubCommand::with_name("use")
+            .about("activate a particular toolchain version")
+            .arg(Arg::with_name("version")
+                .help("Node.js version specifier")
+                .required(false)))
+
+        // nemo current
+        .subcommand(SubCommand::with_name("current")
+            .about("display the current activated toolchain version"))
+
+        // nemo version
+        .subcommand(SubCommand::with_name("version")
+            .about("display the Nemo version"))
+
+        // nemo help
+        .subcommand(SubCommand::with_name("help")
+            .about("display help information"))
+        .get_matches();
+
+    match matches.subcommand_name() {
+        Some("install") => { println!("install!"); }
+        Some("use") => { println!("use!"); }
+        Some("current") => { println!("current!"); }
+        Some("version") => { println!("version!"); }
+        Some("help") => { println!("help!"); }
+        _ => { println!("who knows?"); }
+    }
 }
