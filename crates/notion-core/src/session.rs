9ee68e285cbd3e0b5a370a6fc63f4129ec7f007c
David Herman <david.herman@gmail.com>
2018-02-02 17:06:01 -0800

    eliminate `Location` abstraction, hoist catalog into a lazy cell of `Session`, just store an optional project in session

--- crates/notion-core/src/session.rs
+++ crates/notion-core/src/session.rs
@@ -5,50 +5,24 @@ use version::Version;
 use failure;
 use lazycell::LazyCell;
 
-pub enum Location {
-    Global(Catalog),
-    Local(Project)
-}
-
-impl Location {
-
-    pub fn current() -> Result<Location, failure::Error> {
-        Ok(if let Some(project) = Project::for_current_dir()? {
-            Location::Local(project)
-        } else {
-            Location::Global(Catalog::current()?)
-        })
-    }
-
-    pub fn node_version(&self) -> Result<Option<String>, failure::Error> {
-        match self {
-            &Location::Global(Catalog { node: None }) => {
-                Ok(None)
-            }
-            &Location::Global(Catalog { node: Some(Version::Public(ref version))}) => {
-                Ok(Some(version.clone()))
-            }
-            &Location::Local(ref project) => {
-                Ok(Some(project.lockfile()?.node.version.clone()))
-            }
-        }
-    }
-
-}
-
 pub struct Session {
     config: LazyCell<Config>,
-    location: Location
+    catalog: LazyCell<Catalog>,
+    project: Option<Project>
 }
 
 impl Session {
 
     pub fn new() -> Result<Session, failure::Error> {
-        let location = Location::current()?;
         Ok(Session {
             config: LazyCell::new(),
-            location: location
+            catalog: LazyCell::new(),
+            project: Project::for_current_dir()?
         })
+    }
+
+    pub fn catalog(&self) -> Result<&Catalog, failure::Error> {
+        self.catalog.try_borrow_with(|| Catalog::current())
     }
 
     pub fn config(&self) -> Result<&Config, failure::Error> {
@@ -56,7 +30,18 @@ impl Session {
     }
 
     pub fn node_version(&self) -> Result<Option<String>, failure::Error> {
-        self.location.node_version()
+        if let Some(ref project) = self.project {
+            return Ok(Some(project.lockfile()?.node.version.clone()));
+        }
+
+        match self.catalog()?.node {
+            Some(Version::Public(ref version)) => {
+                Ok(Some(version.clone()))
+            }
+            None => {
+                Ok(None)
+            }
+        }
     }
 
 }
