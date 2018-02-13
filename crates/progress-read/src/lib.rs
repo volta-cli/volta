cd0e78dd2860a8541c13052616ced976540e3282
Dave Herman <david.herman@gmail.com>
2018-02-12 22:10:06 -0800

    comments/API docs for `progress-read` crate

--- crates/progress-read/src/lib.rs
+++ crates/progress-read/src/lib.rs
@@ -1,5 +1,9 @@
+//! This crate provides an adapter for the `std::io::Read` trait to
+//! allow reporting incremental progress to a callback function.
+
 use std::io::{self, Read, Seek, SeekFrom};
 
+/// A reader that reports incremental progress while reading.
 pub struct ProgressRead<R: Read, T, F: FnMut(&T, usize) -> T> {
     source: R,
     accumulator: T,
@@ -7,6 +11,13 @@ pub struct ProgressRead<R: Read, T, F: FnMut(&T, usize) -> T> {
 }
 
 impl<R: Read, T, F: FnMut(&T, usize) -> T> Read for ProgressRead<R, T, F> {
+
+    /// Read some bytes from the underlying reader into the specified buffer,
+    /// and report progress to the progress callback. The progress callback is
+    /// passed the current value of the accumulator as its first argument and
+    /// the number of bytes read as its second argument. The result of the
+    /// progress callback is stored as the updated value of the accumulator,
+    /// to be passed to the next invocation of the callback.
     fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
         let len = self.source.read(buf)?;
         let new_accumulator = {
@@ -16,12 +27,17 @@ impl<R: Read, T, F: FnMut(&T, usize) -> T> Read for ProgressRead<R, T, F> {
         self.accumulator = new_accumulator;
         Ok(len)
     }
+
 }
 
 impl<R: Read, T, F: FnMut(&T, usize) -> T> ProgressRead<R, T, F> {
+
+    /// Construct a new progress reader with the specified underlying reader,
+    /// initial value for an accumulator, and progress callback.
     pub fn new(source: R, init: T, progress: F) -> ProgressRead<R, T, F> {
         ProgressRead { source, accumulator: init, progress }
     }
+
 }
 
 impl<R: Read + Seek, T, F: FnMut(&T, usize) -> T> Seek for ProgressRead<R, T, F> {
