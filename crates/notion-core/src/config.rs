2bb44f8476641bd0bf1ecbd503273fa7c51c316e
Dave Herman <david.herman@gmail.com>
2018-02-21 07:15:07 -1000

    api doc comments for `config` module

--- crates/notion-core/src/config.rs
+++ crates/notion-core/src/config.rs
@@ -12,37 +12,48 @@ use serial::touch;
 use serial;
 use plugin;
 
+/// Lazily loaded Notion configuration settings.
 pub struct LazyConfig {
     config: LazyCell<Config>
 }
 
 impl LazyConfig {
+
+    /// Constructs a new `LazyConfig` (but does not initialize it).
     pub fn new() -> LazyConfig {
         LazyConfig {
             config: LazyCell::new()
         }
     }
 
+    /// Forces the loading of the configuration settings.
     pub fn get(&self) -> Result<&Config, failure::Error> {
         self.config.try_borrow_with(|| Config::current())
     }
 }
 
+/// Notion configuration settings.
 pub struct Config {
     pub node: Option<NodeConfig>
 }
 
+/// Notion configuration settings relating to the Node executable.
 pub struct NodeConfig {
+    /// The plugin for resolving Node versions, if any.
     pub resolve: Option<plugin::Resolve>,
+    /// The plugin for listing the set of Node versions available on the remote server, if any.
     pub ls_remote: Option<plugin::LsRemote>
 }
 
 impl Config {
-    pub fn current() -> Result<Config, failure::Error> {
+
+    /// Returns the current configuration settings, loaded from the filesystem.
+    fn current() -> Result<Config, failure::Error> {
         let path = user_config_file()?;
         let src = touch(&path)?.read_into_string()?;
         src.parse()
     }
+
 }
 
 impl FromStr for Config {
