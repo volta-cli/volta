5af6acb58ac09afd0642739e6c8735a748a7bece
Michael Stewart <mistewart@linkedin.com>
2018-09-06 11:14:52 -0700

    clean up `notion use` tests

--- tests/testsuite/notion_use.rs
+++ tests/testsuite/notion_use.rs
@@ -4,15 +4,38 @@ use support::sandbox::sandbox;
 
 use notion_fail::ExitCode;
 
+const BASIC_PACKAGE_JSON: &'static str = r#"{
+  "name": "test-package"
+}"#;
+
+fn package_json_with_pinned_node(version: &str) -> String {
+    format!(
+        r#"{{
+  "name": "test-package",
+  "toolchain": {{
+    "node": "{}"
+  }}
+}}"#,
+        version
+    )
+}
+
+fn package_json_with_pinned_node_yarn(node_version: &str, yarn_version: &str) -> String {
+    format!(
+        r#"{{
+  "name": "test-package",
+  "toolchain": {{
+    "node": "{}",
+    "yarn": "{}"
+  }}
+}}"#,
+        node_version, yarn_version
+    )
+}
+
 #[test]
 fn use_node() {
-    let s = sandbox()
-        .package_json(
-            r#"{
-  "name": "test-package"
-}"#,
-        )
-        .build();
+    let s = sandbox().package_json(BASIC_PACKAGE_JSON).build();
 
     assert_that(
         s.notion("use node 10"),
@@ -23,24 +46,13 @@ fn use_node() {
 
     assert_eq!(
         s.read_package_json(),
-        r#"{
-  "name": "test-package",
-  "toolchain": {
-    "node": "10.8.0"
-  }
-}"#.to_string()
+        package_json_with_pinned_node("10.8.0"),
     )
 }
 
 #[test]
 fn use_node_latest() {
-    let s = sandbox()
-        .package_json(
-            r#"{
-  "name": "test-package"
-}"#,
-        )
-        .build();
+    let s = sandbox().package_json(BASIC_PACKAGE_JSON).build();
 
     assert_that(
         s.notion("use node latest"),
@@ -51,24 +63,13 @@ fn use_node_latest() {
 
     assert_eq!(
         s.read_package_json(),
-        r#"{
-  "name": "test-package",
-  "toolchain": {
-    "node": "10.8.0"
-  }
-}"#.to_string()
+        package_json_with_pinned_node("10.8.0"),
     )
 }
 
 #[test]
 fn use_yarn_no_node() {
-    let s = sandbox()
-        .package_json(
-            r#"{
-    "name": "test-package"
-}"#,
-        )
-        .build();
+    let s = sandbox().package_json(BASIC_PACKAGE_JSON).build();
 
     assert_that(
         s.notion("use yarn 1.4"),
@@ -77,25 +78,13 @@ fn use_yarn_no_node() {
             .with_stderr_contains("error: There is no pinned node version for this project"),
     );
 
-    assert_eq!(
-        s.read_package_json(),
-        r#"{
-    "name": "test-package"
-}"#.to_string()
-    )
+    assert_eq!(s.read_package_json(), BASIC_PACKAGE_JSON,)
 }
 
 #[test]
 fn use_yarn() {
     let s = sandbox()
-        .package_json(
-            r#"{
-    "name": "test-package",
-    "toolchain": {
-        "node": "1.2.3"
-    }
-}"#,
-        )
+        .package_json(&package_json_with_pinned_node("1.2.3"))
         .build();
 
     assert_that(
@@ -107,27 +96,14 @@ fn use_yarn() {
 
     assert_eq!(
         s.read_package_json(),
-        r#"{
-    "name": "test-package",
-    "toolchain": {
-        "node": "1.2.3",
-        "yarn": "1.4.0"
-    }
-}"#.to_string()
+        package_json_with_pinned_node_yarn("1.2.3", "1.4.0"),
     )
 }
 
 #[test]
 fn use_yarn_latest() {
     let s = sandbox()
-        .package_json(
-            r#"{
-    "name": "test-package",
-    "toolchain": {
-        "node": "1.2.3"
-    }
-}"#,
-        )
+        .package_json(&package_json_with_pinned_node("1.2.3"))
         .build();
 
     assert_that(
@@ -139,12 +115,6 @@ fn use_yarn_latest() {
 
     assert_eq!(
         s.read_package_json(),
-        r#"{
-    "name": "test-package",
-    "toolchain": {
-        "node": "1.2.3",
-        "yarn": "1.2.0"
-    }
-}"#.to_string()
+        package_json_with_pinned_node_yarn("1.2.3", "1.2.0"),
     )
 }
