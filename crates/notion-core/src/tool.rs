d91babb2f80193cb3deaf1169675067f676eeef6
Chuck Pierce <cpierce.grad@gmail.com>
2018-11-28 12:17:36 -0800

    Add error message on npx invocation for old Node

--- crates/notion-core/src/tool.rs
+++ crates/notion-core/src/tool.rs
@@ -11,6 +11,7 @@ use notion_fail::{ExitCode, FailExt, Fallible, NotionError, NotionFail};
 use path;
 use session::{ActivityKind, Session};
 use style;
+use version::VersionSpec;
 
 fn display_error(err: &NotionError) {
     if err.is_user_friendly() {
@@ -400,6 +401,16 @@ impl Tool for Npm {
     }
 }
 
+#[derive(Debug, Fail, NotionFail)]
+#[fail(display = r#"
+'npx' is only available on Node >= 8.2.0
+
+This project is configured to use version {} of Node."#, version)]
+#[notion_fail(code = "ExecutableNotFound")]
+struct NpxNotAvailableError {
+    version: String,
+}
+
 impl Tool for Npx {
     fn new(session: &mut Session) -> Fallible<Self> {
         session.add_event_start(ActivityKind::Npx);
@@ -408,7 +419,16 @@ impl Tool for Npx {
         let exe = arg0(&mut args)?;
         if let Some(ref platform) = session.current_platform()? {
             session.prepare_image(platform)?;
-            Ok(Self::from_components(&exe, args, &platform.path()?))
+
+            // npx was only included with Node >= 8.2.0. If less than that, we should include a helpful error message
+            let required_node = VersionSpec::parse_requirements(">= 8.2.0")?;
+            if required_node.matches(&platform.node) {
+                Ok(Self::from_components(&exe, args, &platform.path()?))
+            } else {
+                throw!(NpxNotAvailableError {
+                    version: platform.node_str.clone()
+                });
+            }
         } else {
             // Using 'Node' as the tool name since the npx version is derived from the Node version
             // This way the error message will prompt the user to add 'Node' to their toolchain, instead of 'npx'
