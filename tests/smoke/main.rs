e9c028955df114a97b71ec731b21cf3d7c0f8917
Michael Stewart <mistewart@linkedin.com>
2018-12-19 11:58:00 -0800

    stop the unused code warnings that show up in the regular tests

--- tests/smoke/main.rs
+++ tests/smoke/main.rs
@@ -12,26 +12,24 @@
 /// different versions of node and yarn.
 #[macro_use]
 extern crate cfg_if;
-extern crate envoy;
-extern crate failure;
-#[macro_use]
-extern crate hamcrest2;
-#[cfg(feature = "mock-network")]
-extern crate mockito;
-extern crate notion_core;
-extern crate notion_fail;
-extern crate rand;
-extern crate reqwest;
-extern crate serde_json;
-#[macro_use]
-extern crate test_support;
-
-mod support;
-
-// test files
 
 cfg_if! {
     if #[cfg(all(unix, feature = "smoke-tests"))] {
+        extern crate envoy;
+        extern crate failure;
+        #[macro_use]
+        extern crate hamcrest2;
+        #[cfg(feature = "mock-network")]
+        extern crate mockito;
+        extern crate notion_core;
+        extern crate notion_fail;
+        extern crate rand;
+        extern crate reqwest;
+        extern crate serde_json;
+        #[macro_use]
+        extern crate test_support;
+
+        mod support;
         mod notion_fetch;
         mod notion_install;
         mod autodownload;
