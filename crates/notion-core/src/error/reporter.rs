9ea343102e3ec6f2addb19d6c3b5fbe2412412ca
Charles Pierce <cpierce.grad@gmail.com>
2019-03-23 11:40:56 -0700

    Write errors to logs

--- crates/notion-core/src/error/reporter.rs
+++ crates/notion-core/src/error/reporter.rs
@@ -1,8 +1,12 @@
-use std::env;
-use std::io::Result;
-use std::path::PathBuf;
+use std::env::{self, args_os};
+use std::fs::File;
+use std::io::Write;
 
+use crate::fs::ensure_containing_dir_exists;
+use crate::path::log_dir;
 use crate::style::{format_error_details, format_error_message};
+use chrono::Local;
+use failure::Error;
 use notion_fail::NotionError;
 
 const NOTION_DEV: &'static str = "NOTION_DEV";
@@ -50,12 +54,37 @@ impl ErrorReporter {
         }
 
         match write_error_log(message, details) {
-            Ok(_log_file) => {}
-            Err(_) => {}
+            Ok(log_file) => {
+                eprintln!("Error log written to: {}", log_file);
+            }
+            Err(_) => {
+                eprintln!("Unable to write error log!");
+            }
         }
     }
 }
 
-fn write_error_log(_message: String, _details: String) -> Result<PathBuf> {
-    Ok(PathBuf::from(""))
+fn write_error_log(message: String, details: String) -> Result<String, Error> {
+    let file_name = Local::now()
+        .format("notion-error-%Y-%m-%d_%H_%M_%S%.3f.log")
+        .to_string();
+    let log_file_path = log_dir()?.join(&file_name);
+
+    ensure_containing_dir_exists(&log_file_path)?;
+    let mut log_file = File::create(log_file_path)?;
+
+    writeln!(log_file, "{}", collect_arguments())?;
+    writeln!(log_file, "Notion v{}", env!("CARGO_PKG_VERSION"))?;
+    writeln!(log_file)?;
+    write!(log_file, "{}{}", message, details)?;
+
+    Ok(file_name)
+}
+
+/// Combines all the arguments into a single String
+fn collect_arguments() -> String {
+    args_os()
+        .map(|arg| arg.into_string().unwrap_or(String::from("<UNKNOWN>")))
+        .collect::<Vec<String>>()
+        .join(" ")
 }
