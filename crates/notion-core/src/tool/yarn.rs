c344b9e8cc5153c04061109b91bf16e19da69977
Chuck Pierce <cpierce.grad@gmail.com>
2019-04-19 16:07:15 -0700

    Don't pass through to Yarn if we have a platform but no Yarn version

--- crates/notion-core/src/tool/yarn.rs
+++ crates/notion-core/src/tool/yarn.rs
@@ -7,7 +7,6 @@ use crate::error::ErrorDetails;
 use crate::platform::PlatformSpec;
 use crate::session::{ActivityKind, Session};
 
-use either::{Either, Left, Right};
 use notion_fail::{throw, Fallible};
 
 pub(super) fn yarn_command<A>(args: A, session: &mut Session) -> Fallible<ToolCommand>
@@ -17,7 +16,7 @@ where
     session.add_event_start(ActivityKind::Yarn);
 
     match get_yarn_platform(session)? {
-        Left(ref platform) => {
+        Some(ref platform) => {
             if intercept_global_installs() && is_global_yarn_add() {
                 throw!(ErrorDetails::NoGlobalInstalls);
             }
@@ -26,29 +25,29 @@ where
             let path = image.path()?;
             Ok(ToolCommand::direct(OsStr::new("yarn"), args, &path))
         }
-        Right(error) => ToolCommand::passthrough(OsStr::new("yarn"), args, error),
+        None => ToolCommand::passthrough(OsStr::new("yarn"), args, ErrorDetails::NoPlatform),
     }
 }
 
 /// Determine the correct platform (project or user) and check if yarn is set for that platform
-fn get_yarn_platform(session: &mut Session) -> Fallible<Either<Rc<PlatformSpec>, ErrorDetails>> {
+fn get_yarn_platform(session: &mut Session) -> Fallible<Option<Rc<PlatformSpec>>> {
     // First check if we are in a pinned project
     if let Some(platform) = session.project_platform()? {
         return match platform.yarn {
-            Some(_) => Ok(Left(platform)),
-            None => Ok(Right(ErrorDetails::NoProjectYarn)),
+            Some(_) => Ok(Some(platform)),
+            None => Err(ErrorDetails::NoProjectYarn.into()),
         };
     }
 
     // If not, fall back to the user platform
     if let Some(platform) = session.user_platform()? {
         return match platform.yarn {
-            Some(_) => Ok(Left(platform)),
-            None => Ok(Right(ErrorDetails::NoUserYarn)),
+            Some(_) => Ok(Some(platform)),
+            None => Err(ErrorDetails::NoUserYarn.into()),
         };
     }
 
-    Ok(Right(ErrorDetails::NoPlatform))
+    Ok(None)
 }
 
 fn is_global_yarn_add() -> bool {
