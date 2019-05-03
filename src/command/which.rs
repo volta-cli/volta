b49a23bc2cf5c24ba7c871a649bbf687d670aa91
Chris Krycho <hello@chriskrycho.com>
2019-05-03 07:36:04 -0600

    Cleanup: 'cleaner' code in `which` implementation.

--- src/command/which.rs
+++ src/command/which.rs
@@ -23,7 +23,6 @@ impl Command for Which {
         // Treat any error with obtaining the current platform image as if the image doesn't exist
         // However, errors in obtaining the current working directory or the System path should
         // still be treated as errors.
-        let cwd = env::current_dir().with_context(|_| ErrorDetails::CurrentDirError)?;
         let path = match session
             .current_platform()
             .unwrap_or(None)
@@ -34,20 +33,21 @@ impl Command for Which {
             None => System::path()?,
         };
 
-        match which_in(&self.binary, Some(path), cwd) {
+        let cwd = env::current_dir().with_context(|_| ErrorDetails::CurrentDirError)?;
+        let exit_code = match which_in(&self.binary, Some(path), cwd) {
             Ok(result) => {
                 println!("{}", result.to_string_lossy());
-                session.add_event_end(ActivityKind::Which, ExitCode::Success);
-
-                Ok(ExitCode::Success)
+                ExitCode::Success
             }
             Err(_) => {
                 // `which_in` Will return an Err if it can't find the binary in the path
                 // In that case, we don't want to print anything out, but we want to return
                 // Exit Code 1 (ExitCode::UnknownError)
-                session.add_event_end(ActivityKind::Which, ExitCode::UnknownError);
-                Ok(ExitCode::UnknownError)
+                ExitCode::UnknownError
             }
-        }
+        };
+
+        session.add_event_end(ActivityKind::Which, exit_code);
+        Ok(exit_code)
     }
 }
