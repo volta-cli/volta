ed8b53498526a662552b734e68f6364b50e87cbd
Michael Stewart <mistewart@linkedin.com>
2019-06-19 11:55:00 -0700

    parse input requirements in node-compatible mode

--- crates/volta-core/src/version/serial.rs
+++ crates/volta-core/src/version/serial.rs
@@ -1,15 +1,15 @@
-use semver::{ReqParseError, VersionReq};
+use semver::{Compat, ReqParseError, VersionReq};
 
 pub fn parse_requirements(src: &str) -> Result<VersionReq, ReqParseError> {
     let src = src.trim();
     if src.len() > 0 && src.chars().next().unwrap().is_digit(10) {
         let defaulted = format!("={}", src);
-        VersionReq::parse(&defaulted)
+        VersionReq::parse_compat(&defaulted, Compat::Node)
     } else if src.len() > 0 && src.chars().next().unwrap() == 'v' {
         let defaulted = src.replacen("v", "=", 1);
-        VersionReq::parse(&defaulted)
+        VersionReq::parse_compat(&defaulted, Compat::Node)
     } else {
-        VersionReq::parse(src)
+        VersionReq::parse_compat(src, Compat::Node)
     }
 }
 
@@ -17,29 +17,29 @@ pub fn parse_requirements(src: &str) -> Result<VersionReq, ReqParseError> {
 pub mod tests {
 
     use crate::version::serial::parse_requirements;
-    use semver::VersionReq;
+    use semver::{Compat, VersionReq};
 
     #[test]
     fn test_parse_requirements() {
         assert_eq!(
             parse_requirements("1.2.3").unwrap(),
-            VersionReq::parse("=1.2.3").unwrap()
+            VersionReq::parse_compat("=1.2.3", Compat::Node).unwrap()
         );
         assert_eq!(
             parse_requirements("v1.5").unwrap(),
-            VersionReq::parse("=1.5").unwrap()
+            VersionReq::parse_compat("=1.5", Compat::Node).unwrap()
         );
         assert_eq!(
             parse_requirements("=1.2.3").unwrap(),
-            VersionReq::parse("=1.2.3").unwrap()
+            VersionReq::parse_compat("=1.2.3", Compat::Node).unwrap()
         );
         assert_eq!(
             parse_requirements("^1.2").unwrap(),
-            VersionReq::parse("^1.2").unwrap()
+            VersionReq::parse_compat("^1.2", Compat::Node).unwrap()
         );
         assert_eq!(
             parse_requirements(">=1.4").unwrap(),
-            VersionReq::parse(">=1.4").unwrap()
+            VersionReq::parse_compat(">=1.4", Compat::Node).unwrap()
         );
     }
 }
