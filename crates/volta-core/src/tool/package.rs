2c3316b03e7b191da5728f804e1ae8661c7b8764
Chuck Pierce <cpierce.grad@gmail.com>
2019-07-30 10:20:15 -0700

    Refactor the uninstall process for orphaned binaries

--- crates/volta-core/src/tool/package.rs
+++ crates/volta-core/src/tool/package.rs
@@ -72,12 +72,8 @@ pub fn uninstall(name: &str) -> Fallible<()> {
             .with_context(delete_file_error(&package_config_file))?;
     } else {
         // there is no package config - check for orphaned binaries
-        let user_bin_dir = path::user_bin_dir()?;
-        if user_bin_dir.exists() {
-            let orphaned_bins = binaries_from_package(name)?;
-            for bin_name in orphaned_bins {
-                remove_config_and_shim(&bin_name, name)?;
-            }
+        for bin_name in binaries_from_package(name)? {
+            remove_config_and_shim(&bin_name, name)?;
         }
     }
 
@@ -106,16 +102,20 @@ fn remove_config_and_shim(bin_name: &str, pkg_name: &str) -> Fallible<()> {
 /// all the binaries installed by the input package.
 fn binaries_from_package(package: &str) -> Fallible<Vec<String>> {
     let bin_config_dir = path::user_bin_dir()?;
-    dir_entry_match(&bin_config_dir, |entry| {
-        let path = entry.path();
-        if let Ok(config) = BinConfig::from_file(path) {
-            if config.package == package.to_string() {
-                return Some(config.name);
-            }
-        };
-        None
-    })
-    .with_context(|_| ErrorDetails::ReadBinConfigDirError {
-        dir: bin_config_dir,
-    })
+    if bin_config_dir.exists() {
+        dir_entry_match(&bin_config_dir, |entry| {
+            let path = entry.path();
+            if let Ok(config) = BinConfig::from_file(path) {
+                if config.package == package.to_string() {
+                    return Some(config.name);
+                }
+            };
+            None
+        })
+        .with_context(|_| ErrorDetails::ReadBinConfigDirError {
+            dir: bin_config_dir,
+        })
+    } else {
+        Ok(vec![])
+    }
 }
