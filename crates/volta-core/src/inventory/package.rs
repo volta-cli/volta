b476a3b23d698ee9382bd688f5d41ae3725a28c9
Chris Krycho <hello@chriskrycho.com>
2019-08-01 17:22:39 -0600

    volta list: partially implement `PackageCollection::load`
    `PackageCollection::load` now correctly loads the package configs for
    all items in the first level of the relevant directory. It does not
    yet recurse into child directories, and therefore does not yet support
    namespaced packages.

--- crates/volta-core/src/inventory/package.rs
+++ crates/volta-core/src/inventory/package.rs
@@ -1,9 +1,17 @@
 use std::collections::BTreeSet;
+use std::ffi::OsString;
+use std::fs::{read, read_dir, DirEntry, File};
+use std::io::Read;
+use std::path::PathBuf;
 
+use failure::ResultExt;
+use log::debug;
 use semver::Version;
 
-use volta_fail::Fallible;
+use volta_fail::{Fallible, VoltaError};
 
+use crate::error::ErrorDetails;
+use crate::path;
 use crate::tool::{Package, PackageConfig};
 
 // Convenience for access as `package::Collection`
@@ -17,9 +25,56 @@ impl PackageCollection {
     // loads an empty PackageCollection
     // ISSUE(#288) Collection only supports versions - for packages we also need names
     pub(crate) fn load() -> Fallible<Self> {
-        Ok(PackageCollection {
-            packages: BTreeSet::new(),
-        })
+        let package_dir = path::user_package_dir()?;
+
+        let dir_contents = read_dir(&package_dir)
+            .with_context(|_| ErrorDetails::ReadInventoryDirError { dir: package_dir })?;
+
+        let file_paths = dir_contents
+            // Ignore any items which didn't resolve as `DirEntry` correctly.
+            // There is no point trying to do anything with those, and no error
+            // we can report to the user in any case. Log the failure in the
+            // debug output, though
+            .filter_map(|entry| match entry {
+                Ok(dir_entry) => Some(dir_entry),
+                Err(e) => {
+                    debug!("{}", e);
+                    None
+                }
+            })
+            // Ignore directory entries.
+            // TODO: *recurse* through directories before we get to this point,
+            //       so that we correctly handle namespaced packages.
+            .filter_map(|dir_entry| {
+                let is_file = dir_entry
+                    .file_type()
+                    .map(|file_type| file_type.is_dir())
+                    .unwrap_or(false);
+
+                if is_file {
+                    Some(PathBuf::from(dir_entry.file_name()))
+                } else {
+                    None
+                }
+            })
+            .collect::<Vec<PathBuf>>();
+
+        // Note: this approach fails eagerly -- if *any* of the packages
+        //       installed error out on deserialization, we die immediately and
+        //       report to the user.
+        let mut packages: BTreeSet<PackageConfig> = BTreeSet::new();
+        for file_path in file_paths {
+            let file =
+                File::open(&file_path).with_context(|_| ErrorDetails::ReadPackageConfigError {
+                    file: file_path.clone(),
+                })?;
+
+            let config = PackageConfig::from_file(&file_path)?;
+
+            packages.insert(config);
+        }
+
+        Ok(PackageCollection { packages })
     }
 
     pub(crate) fn contains(&self, name: &str, version: &Version) -> bool {
