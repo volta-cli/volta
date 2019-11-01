22492f38d2e1cefa8ee6fd8bec0a14adc8aaeec1
Chuck Pierce <cpierce.grad@gmail.com>
2019-11-01 15:54:39 -0700

    [volta updates] Restrict Unix-only migrations to unix builds

--- crates/volta-migrate/src/v1.rs
+++ crates/volta-migrate/src/v1.rs
@@ -65,33 +65,36 @@ impl TryFrom<V0> for V1 {
                 dir: new_home.root().to_owned(),
             })?;
 
-        debug!("Removing unnecessary 'load.*' files");
-        let root_contents =
-            read_dir_eager(new_home.root()).with_context(|_| ErrorDetails::ReadDirError {
-                dir: new_home.root().to_owned(),
-            })?;
-        for (entry, _) in root_contents {
-            let path = entry.path();
-            if let Some(stem) = path.file_stem() {
-                if stem == "load" && path.is_file() {
-                    remove_file(&path)
-                        .with_context(|_| ErrorDetails::DeleteFileError { file: path })?;
+        #[cfg(unix)]
+        {
+            debug!("Removing unnecessary 'load.*' files");
+            let root_contents =
+                read_dir_eager(new_home.root()).with_context(|_| ErrorDetails::ReadDirError {
+                    dir: new_home.root().to_owned(),
+                })?;
+            for (entry, _) in root_contents {
+                let path = entry.path();
+                if let Some(stem) = path.file_stem() {
+                    if stem == "load" && path.is_file() {
+                        remove_file(&path)
+                            .with_context(|_| ErrorDetails::DeleteFileError { file: path })?;
+                    }
                 }
             }
-        }
 
-        debug!("Removing old Volta binaries");
-        let old_volta_bin = new_home.root().join("volta");
-        if old_volta_bin.exists() {
-            remove_file(&old_volta_bin).with_context(|_| ErrorDetails::DeleteFileError {
-                file: old_volta_bin,
-            })?;
-        }
+            debug!("Removing old Volta binaries");
+            let old_volta_bin = new_home.root().join("volta");
+            if old_volta_bin.exists() {
+                remove_file(&old_volta_bin).with_context(|_| ErrorDetails::DeleteFileError {
+                    file: old_volta_bin,
+                })?;
+            }
 
-        let old_shim_bin = new_home.root().join("shim");
-        if old_shim_bin.exists() {
-            remove_file(&old_shim_bin)
-                .with_context(|_| ErrorDetails::DeleteFileError { file: old_shim_bin })?;
+            let old_shim_bin = new_home.root().join("shim");
+            if old_shim_bin.exists() {
+                remove_file(&old_shim_bin)
+                    .with_context(|_| ErrorDetails::DeleteFileError { file: old_shim_bin })?;
+            }
         }
 
         Ok(V1 { home: new_home })
