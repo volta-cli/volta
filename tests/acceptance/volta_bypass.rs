55f483551619f743bb42a5a3d04748b9c9c393c8
Charles Pierce <cpierce.grad@gmail.com>
2019-11-12 16:04:33 -0800

    Fix test behavior on Windows

--- tests/acceptance/volta_bypass.rs
+++ tests/acceptance/volta_bypass.rs
@@ -7,12 +7,27 @@ use volta_fail::ExitCode;
 
 #[test]
 fn shim_skips_platform_checks_on_bypass() {
-    let s = sandbox().env("VOLTA_BYPASS", "1").build();
+    let s = sandbox()
+        .env("VOLTA_BYPASS", "1")
+        .env(
+            "VOLTA_INSTALL_DIR",
+            &shim_exe().parent().unwrap().to_string_lossy(),
+        )
+        .build();
 
+    #[cfg(unix)]
     assert_that!(
         s.process(&shim_exe()),
         execs()
             .with_status(ExitCode::ExecutionFailure as i32)
             .with_stderr_contains("VOLTA_BYPASS is enabled[..]")
+    );
+
+    #[cfg(windows)]
+    assert_that!(
+        s.process(&shim_exe()),
+        execs()
+            .with_status(ExitCode::UnknownError as i32)
+            .with_stderr_contains("[..]is not recognized as an internal or external command[..]")
     );
 }
