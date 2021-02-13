278c979fd9481db1a42d6d4e70cb24e8386688f0
Tomoki Aonuma <uasi@uasi.jp>
2021-02-13 15:16:51 +0900

    Add --no-create option to volta-migrate

--- src/volta-migrate.rs
+++ src/volta-migrate.rs
@@ -1,10 +1,41 @@
+use structopt::StructOpt;
+
 use volta_core::error::{report_error, ExitCode};
+use volta_core::layout::volta_home;
 use volta_core::log::{LogContext, LogVerbosity, Logger};
 use volta_migrate::run_migration;
 
+#[derive(StructOpt)]
+#[structopt(
+    name = "volta-migrate",
+    about = "Migrates the Volta directory to the latest version",
+    raw(global_setting = "structopt::clap::AppSettings::ColoredHelp"),
+    raw(global_setting = "structopt::clap::AppSettings::ColorAuto"),
+    raw(global_setting = "structopt::clap::AppSettings::DeriveDisplayOrder"),
+    raw(global_setting = "structopt::clap::AppSettings::DisableVersion")
+)]
+struct VoltaMigrate {
+    #[structopt(
+        long = "no-create",
+        help = "Runs migration only if the Volta directory already exists",
+        global = true
+    )]
+    pub(crate) no_create: bool,
+}
+
 pub fn main() {
+    let volta_migrate = VoltaMigrate::from_args();
+
     Logger::init(LogContext::Migration, LogVerbosity::Default)
         .expect("Only a single Logger should be initialized");
+
+    if volta_migrate.no_create
+        && !volta_home()
+            .map(|home| home.root().exists())
+            .unwrap_or(false)
+    {
+        ExitCode::Success.exit();
+    }
 
     let exit_code = match run_migration() {
         Ok(()) => ExitCode::Success,
