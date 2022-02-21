ede9d6bbf170cfa17f86a48492a174bd853f87a3
Jens Meindertsma <mail@jensmeindertsma.com>
2022-02-21 17:41:05 +0100

    Changes

--- ci/build-and-package.sh
+++ ci/build-and-package.sh
@@ -4,9 +4,14 @@ set -e
 
 echo "Building Volta"
 
-cargo build --release
+if [ -z "$2" ]; then 
+    cargo build --release
+    cd target/release
+else 
+    cargo build --release --target=$2
+    cd target/$2/release
+fi
 
 echo "Packaging Binaries"
 
-cd target/release
 tar -zcvf "$1.tar.gz" volta volta-shim volta-migrate
--- ci/build-with-openssl.sh
+++ ci/build-with-openssl.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 set -e
-OPENSSL_DIR=/app/src/openssl-dist ./ci/build-and-package.sh "$1"
+OPENSSL_DIR=/app/src/openssl-dist ./ci/build-and-package.sh "$1" "$2"
