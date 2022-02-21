fd4793bb33f2a26e24db01e96f8d5b8e1fe965da
Jens Meindertsma <mail@jensmeindertsma.com>
2022-02-21 23:44:51 +0100

    cargo cross doesn't help

--- ci/build-and-package.sh
+++ ci/build-and-package.sh
@@ -4,9 +4,19 @@ set -e
 
 echo "Building Volta"
 
-cargo build --release
+if [[ "$1" == *linux-musl* ]]; then
+    if [[ "$1" == *aarch64* ]]; then
+        cross build --release --target aarch64-unknown-linux-musl
+        cd target/aarch64-unknown-linux-musl/release
+    elif [[ "$1" == *x86_64* ]]; then
+        cross build --release --target x86_64-unknown-linux-musl
+        cd target/x86_64-unknown-linux-musl/release
+    fi
+else
+    cargo build --release
+    cd target/release
+fi
 
 echo "Packaging Binaries"
 
-cd target/release
 tar -zcvf "$1.tar.gz" volta volta-shim volta-migrate
