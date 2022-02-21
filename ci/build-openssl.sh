cfce2abaf19f3467c316166b38ebc9bcbd8b2b8a
Jens Meindertsma <mail@jensmeindertsma.com>
2022-02-21 13:07:14 +0100

    Improve cacheability

--- ci/build-openssl.sh
+++ ci/build-openssl.sh
@@ -2,9 +2,6 @@
 
 set -e
 
-echo "Checking out OpenSSL $1"
-git clone --branch OpenSSL_"$1"-stable https://github.com/openssl/openssl
-
 echo "Building OpenSSL"
 cd openssl
 ./config shared --prefix=/app/src/openssl-dist
