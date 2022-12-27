7e8f89c9ca8075e389637b338ccd34f871bd384b
Thomas Wang <hi@xgwang.me>
2022-12-26 18:02:39 -0800

    cargo fmt

--- crates/volta-core/src/run/binary.rs
+++ crates/volta-core/src/run/binary.rs
@@ -35,7 +35,10 @@ pub(super) fn command(exe: &OsStr, args: &[OsString], session: &mut Session) -> 
                 }
                 None => {
                     if project.needs_yarn_run() {
-                        debug!("Project needs to use yarn to run command, calling {} with 'yarn'", bin);
+                        debug!(
+                            "Project needs to use yarn to run command, calling {} with 'yarn'",
+                            bin
+                        );
                         let platform = Platform::current(session)?;
                         let mut exe_and_args = vec![exe.to_os_string()];
                         exe_and_args.extend_from_slice(args);
