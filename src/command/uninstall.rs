97a09a67cf7e0270291dea1925cdf5896bea11cb
MrChocolatine <47531779+MrChocolatine@users.noreply.github.com>
2023-12-26 17:50:42 +0100

    refactor: update `uninstall` help text
    With examples of what can actually be uninstalled.

--- src/command/uninstall.rs
+++ src/command/uninstall.rs
@@ -9,7 +9,7 @@ use crate::command::Command;
 
 #[derive(StructOpt)]
 pub(crate) struct Uninstall {
-    /// The tool to uninstall, e.g. <package>
+    /// The tool to uninstall, like `ember-cli-update`, `typescript`, or <package>
     tool: String,
 }
 
