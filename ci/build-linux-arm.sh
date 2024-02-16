691b39e4c7f0f7702219c4a4dd6b127b7b6a0c24
Charles Pierce <charlespierce@users.noreply.github.com>
2024-02-16 12:33:09 -0800

    Testing potential ARM Linux build with cross

---
+++ ci/build-linux-arm.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+# Activate the upgraded versions of GCC and binutils
+# See https://linux.web.cern.ch/centos7/docs/softwarecollections/#inst
+source /opt/rh/devtoolset-8/enable
+
+echo "Building Volta"
+
+cross build --release --target aarch64-unknown-linux-gnu
+
+echo "Packaging Binaries"
+
+cd target/release
+tar -zcvf "$1.tar.gz" volta volta-shim volta-migrate
