081e2b38524837edb4d17dc1ea132043d0e06081
Xavier Stouder <xavier@stouder.io>
2024-03-22 20:50:12 +0100

    fix: bubble up the correct exit code

--- src/volta-shim.rs
+++ src/volta-shim.rs
@@ -3,7 +3,10 @@ use std::process::{Command, ExitCode};
 pub fn main() -> ExitCode {
     let result = Command::new("volta-runner").args(std::env::args()).status();
     match result {
-        Ok(_) => ExitCode::code,
+        Ok(exit_status) => match exit_status.code() {
+            None => ExitCode::FAILURE,
+            Some(code) => ExitCode::from(code as u8)
+        },
         Err(_) => ExitCode::FAILURE,
     }
 }
