bea9d215818273352a18d9bda9a3fb51fcadc617
YZH <cong949654900@gmail.com>
2024-08-20 16:59:04 +0800

    Enhance uninstall node

--- src/command/uninstall.rs
+++ src/command/uninstall.rs
@@ -1,21 +1,23 @@
 use volta_core::error::{ErrorKind, ExitCode, Fallible};
 use volta_core::session::{ActivityKind, Session};
-use volta_core::tool;
+use volta_core::tool::{self, Spec};
 
 use crate::command::Command;
 
 #[derive(clap::Args)]
 pub(crate) struct Uninstall {
-    /// The tool to uninstall, like `ember-cli-update`, `typescript`, or <package>
-    tool: String,
+    /// Tools to uninstall, like `node`, `yarn@latest` or `your-package`.
+    #[arg(value_name = "tool[@version]", required = true)]
+    tools: Vec<String>,
 }
 
 impl Command for Uninstall {
     fn run(self, session: &mut Session) -> Fallible<ExitCode> {
         session.add_event_start(ActivityKind::Uninstall);
 
-        let tool = tool::Spec::try_from_str(&self.tool)?;
-        tool.resolve(session)?.uninstall(session)?;
+        for tool in Spec::from_strings(&self.tools, "uninstall")? {
+            tool.resolve(session)?.uninstall(session)?;
+        }
 
         session.add_event_end(ActivityKind::Uninstall, ExitCode::Success);
         Ok(ExitCode::Success)
